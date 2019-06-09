615b530ad747ce023276463b02fc826ad8e9cc63
James O'Shea <j.oshea@holidaypirates.com>
2019-06-09 14:55:59 +0200

    horrible code to populate DB

--- main.go
+++ main.go
@@ -4,13 +4,36 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-
 	"preposterous/controllers"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+
+	// filename := "verben.csv"
+
+	// f, err := os.Open(filename)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer f.Close()
+
+	// lines, err := csv.NewReader(f).ReadAll()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// for _, line := range lines {
+	// 	verb := &models.Verb{
+	// 		German:          line[0],
+	// 		Preposition:     line[1],
+	// 		GrammaticalCase: line[2],
+	// 		English:         line[3],
+	// 	}
+	// 	verb.Create()
+	// }
+
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api/verb/new", controllers.CreateVerb).Methods("POST")
@@ -25,7 +48,7 @@ func main() {
 
 	fmt.Println(port)
 
-	err := http.ListenAndServe(":"+port, router)
+	err = http.ListenAndServe(":"+port, router)
 	if err != nil {
 		fmt.Print(err)
 	}
